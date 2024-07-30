d790e42eaa6111137f719141ff4c64c243e9f563
Ezio660914 <m202372822@hust.edu.cn>
2024-07-30 18:54:42 +0800

    initial commit

---
+++ SaveOurShipPatch/Command_DeconstructShipEnemy.cs
@@ -0,0 +1,99 @@
+﻿using RimWorld;
+using Verse;
+using SaveOurShip2;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using UnityEngine;
+
+
+namespace SaveOurShipPatch
+{
+    public class Command_DeconstructShipEnemy : Command
+    {
+        private Map player_map;
+        private Map salvage_map;
+        public Command_DeconstructShipEnemy(Map player_map, Map salvage_map) : base()
+        {
+            this.player_map = player_map;
+            this.salvage_map = salvage_map;
+        }
+        public override void ProcessInput(Event ev)
+        {
+            base.ProcessInput(ev);
+            ConvertShipToResources();
+        }
+
+        public void ConvertShipToResources()
+        {
+            ShipMapComp targetMapComp = salvage_map.GetComponent<ShipMapComp>();
+            //Count all buildings costs in enemy ship map
+            HashSet<IntVec3> area = new HashSet<IntVec3>();
+            foreach (var ship_cache in targetMapComp.ShipsOnMap.Values)
+            {
+                area.AddRange(ship_cache.Area);
+            }
+            List<Thing> things = new List<Thing>();
+            foreach (IntVec3 pos in area)
+            {
+                foreach (Thing t in pos.GetThingList(salvage_map))
+                {
+                    if (!(t is Pawn) && !things.Contains(t))
+                    {
+                        things.Add(t);
+                    }
+                }
+            }
+            Dictionary<ThingDef, int> cost_list = new Dictionary<ThingDef, int>();
+            foreach (Thing t in things)
+            {
+                try
+                {
+                    if (t is Building && t.def.destroyable && !t.Destroyed)
+                    {
+                        foreach (var cost in t.def.costList)
+                        {
+                            if (!cost_list.ContainsKey(cost.thingDef))
+                            {
+                                cost_list[cost.thingDef] = 0;
+                            }
+                            cost_list[cost.thingDef] += cost.count;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Warning("" + e);
+                }
+            }
+            //send all resources to player's map using pod
+            List<Thing> thing_list = new List<Thing>();
+            string drop_pod_received_text = "";
+            foreach (var thing_def_count in cost_list)
+            {
+                Thing thing = ThingMaker.MakeThing(thing_def_count.Key, null);
+                thing.stackCount = thing_def_count.Value;
+                TradeUtility.SpawnDropPod(DropCellFinder.TradeDropSpot(player_map), player_map, thing);
+                drop_pod_received_text += thing_def_count.Key.label + ": " + thing_def_count.Value.ToString() + "\n";
+                thing_list.Add(thing);
+            }
+            Find.LetterStack.ReceiveLetter(
+                    TranslatorFormattedStringExtensions.Translate("DropPodReceivedLabel"),
+                    TranslatorFormattedStringExtensions.Translate("DropPodReceivedText", drop_pod_received_text),
+                    LetterDefOf.PositiveEvent, thing_list, null, null, null, null
+                    );
+            //destroy all ships in that map
+            try
+            {
+                foreach (var ship_index in targetMapComp.ShipsOnMap.Keys)
+                {
+                    ShipInteriorMod2.RemoveShipOrArea(salvage_map, ship_index);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Warning("" + e);
+            }
+        }
+    }
+}
---
+++ SaveOurShipPatch/CompShipBaySalvageAdvanced.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using Verse;
+using SaveOurShip2;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace SaveOurShipPatch
+{
+    public class CompShipBaySalvageAdvanced : CompShipBaySalvage
+    {
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            foreach (Gizmo item in base.CompGetGizmosExtra())
+            {
+                yield return item;
+            }
+            bool nominal = mapComp.ShipMapState == ShipMapState.nominal;
+            foreach (Map map in Find.Maps)
+            {
+                if (map == mapComp.map)
+                    continue;
+                ShipMapComp targetMapComp = map.GetComponent<ShipMapComp>();
+                if (targetMapComp.ShipMapState != ShipMapState.isGraveyard)
+                    continue;
+                if (targetMapComp.MapShipCells.Any())
+                {
+                    var deconstructShipEnemy = new Command_DeconstructShipEnemy(mapComp.map, map)
+                    {
+                        icon = ContentFinder<Texture2D>.Get("UI/SalvageDeconstruct"),
+                        defaultLabel = TranslatorFormattedStringExtensions.Translate("SalvageDeconstructCommand", map.Parent.Label),
+                        defaultDesc = TranslatorFormattedStringExtensions.Translate("SalvageDeconstructCommandDesc", map.Parent.Label),
+                    };
+                    if (!nominal)
+                    {
+                        deconstructShipEnemy.Disable(TranslatorFormattedStringExtensions.Translate("SalvageDeconstructDisabled"));
+                    }
+                    yield return deconstructShipEnemy;
+                }
+            }
+
+
+        }
+    }
+}
